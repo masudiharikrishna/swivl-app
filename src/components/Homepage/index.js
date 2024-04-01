923b675e954e48fb28fa85dc4ccb55c6b4467481
masudiharikrishna <harikrishnamasudi@gmail.com>
2024-04-01 16:17:53 +0530

    homepage completed

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,13 +1,163 @@
-import react from "react";
-import "./index.css"
+import React from "react";
+import "./index.css";
+import { RiDashboardFill } from "react-icons/ri";
+import { BsTags } from "react-icons/bs";
+import { TbCalendarTime } from "react-icons/tb";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { IoSettingsOutline } from "react-icons/io5";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { BsEnvelopePaper } from "react-icons/bs";
+import { AiOutlineLike } from "react-icons/ai";
+import { LuUsers2 } from "react-icons/lu";
+import { Chart } from "react-google-charts";
+import { Pie } from 'react-chartjs-2';
 
-class Homepage extends react.Component{
-    render(){
-        return(
+class Homepage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      populationData: [],
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
+      .then((response) => response.json())
+      .then((data) => {
+        const populationData = data.data.map((item) => [
+          item.Year,
+          item.Population
+        ]);
+        this.setState({ populationData, loading: false });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        this.setState({ loading: false });
+      });
+  }
+
+  render() {
+    return (
+      <div className="homepage">
+        <div className="dashboard">
+          <div>
+            <h1>Board.</h1>
+          </div>
+          <ul className="dashboard-list">
+          <li className="list-item">
+              <RiDashboardFill size={20} />
+              <p>Dashboard</p>
+            </li>
+            <li className="list-item">
+              <BsTags size={20} />
+              <p>Transactions</p>
+            </li>
+            <li className="list-item">
+              <TbCalendarTime size={20} />
+              <p>Schedules</p>
+            </li>
+            <li className="list-item">
+              <FaRegCircleUser size={20} />
+              <p>Users</p>
+            </li>
+            <li className="list-item">
+              <IoSettingsOutline size={20} />
+              <p>Settings</p>
+            </li>
+          </ul>
+          <ul className="help-section">
+            <li>Help</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <div className="charts-section">
+          <div className="nav-bar">
+            <h1>Dashboard</h1>
             <div>
-                <h1>Homepage</h1>
+              <input type="search" className="search-input" />
+              <IoMdNotificationsOutline size={25} className="icon" />
             </div>
-        )
-    }
+          </div>
+          <ul className="total-list-container">
+          <li className="revenue-list-item">
+              <BsEnvelopePaper size={30} className="list-icon" color="black" />
+              <p>Total Revenues</p>
+              <h3>$2,129,430</h3>
+            </li>
+            <li className="revenue-list-item transcation">
+              <BsTags size={30} className="list-icon" color="black" />
+              <p>Total Transactions</p>
+              <h3>1,520</h3>
+            </li>
+            <li className="revenue-list-item likes">
+              <AiOutlineLike size={30} className="list-icon" color="black" />
+              <p>Total Likes</p>
+              <h3>9,721</h3>
+            </li>
+            <li className="revenue-list-item users">
+              <LuUsers2 size={30} className="list-icon" color="black" />
+              <p>Total Users</p>
+              <h3>892</h3>
+            </li>
+          </ul>
+          {this.state.loading ? (
+            <div>Loading...</div>
+          ) : (
+            <Chart
+              width={'98%'}
+              height={'300px'}
+              chartType="Line"
+              className="charts"
+              loader={<div>Loading Chart</div>}
+              data={[
+                ['United States', 'Population'],
+                ...this.state.populationData
+              ]}
+              options={{
+                title: 'Population by Country',
+              }}
+              rootProps={{ 'data-testid': '1' }}
+            />
+          )}
+          <div className="last-container">
+              <div className="chart-container">
+              <Chart
+              width={'100%'}
+              height={'300px'}
+              chartType="PieChart"
+              className="charts"
+              loader={<div>Loading Chart</div>}
+              data={[
+                ['United States', 'Population'],
+                ...this.state.populationData
+              ]}
+              options={{
+                title: 'Population in United States',
+              }}
+              rootProps={{ 'data-testid': '1' }}
+            />  
+              </div>
+              <div className="meeting-schedule-container">
+                <div className="schedule-heading">
+                  <h1>Today's schedule</h1>
+                  <p>See All ></p>
+                </div>
+                <div className="meeting-list">
+                  <h3>Meeting with suppliers from Kuta Bali</h3>
+                  <p>14.00- 15.00</p>
+                  <p>at Sunset Road, Kuta, Bali</p>
+                </div>
+                <div className="meeting-list2">
+                  <h3>Meeting with suppliers from Kuta Bali</h3>
+                  <p>14.00- 15.00</p>
+                  <p>at Sunset Road, Kuta, Bali</p>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-export default Homepage
+export default Homepage;
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,40 +1,55 @@
-import react from "react";
-import "./index.css"
+import React from "react";
+import "./index.css";
+import { GoogleLogin } from "react-google-login";
 
-class Login extends react.Component{
-    render(){
-        return(
-            <div className="Login-page">
-                <div className="board-section">
-                    <h1>Board.</h1>
-                </div>
-                <div className="sign-in-section">
-                    <div className="sign-in-item">
-                        <h1>Sign In</h1>
-                        <h4>Sign in to your account</h4>
-                        <div className="sign-part">
-                            <div className="sign-in-google">
-                                <img src="https://res.cloudinary.com/dmofgf8v0/image/upload/v1701884547/download_tgrebg.png" className="logo" alt="google"/>
-                                <p>Sign in with Google</p>
-                            </div>
-                            <div className="sign-in-google">
-                                <img src="https://res.cloudinary.com/dmofgf8v0/image/upload/v1701884558/Apple-Logo_luonxp.png" className="logo-apple" alt="apple"/>
-                                <p>Sign in with Apple</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="login-part">
-                        <label>Email address</label>
-                        <input type="mail" className="mail-input"/>
-                        <label>Password</label>
-                        <input type="password" className="password-input"/>
-                        <p>Forgot Password ?</p>
-                        <button>Sign In</button>
-                    </div>
-                    <p>Don't have an account? <span className="register">Register here</span></p>
-                </div>
+class Login extends React.Component {
+    responseGoogle = (response) => {
+        console.log(response);
+      };
+  render() {
+    return (
+      <div className="Login-page">
+        <div className="board-section">
+          <h1>Board.</h1>
+        </div>
+        <div className="sign-in-section">
+          <div className="sign-in-item">
+            <h1>Sign In</h1>
+            <h4>Sign in to your account</h4>
+            <div className="sign-part">
+              <div className="sign-in-google">
+              <GoogleLogin
+                clientId="363065887324-jajlb12dl55ud30o1mu2156fbu6mugon.apps.googleusercontent.com"
+                buttonText="Sign in with Google"
+                onSuccess={this.responseGoogle}
+                onFailure={this.responseGoogle}
+                cookiePolicy={"single_host_origin"}
+              />
+              </div>
+              <div className="sign-in-google">
+                <img
+                  src="https://res.cloudinary.com/dmofgf8v0/image/upload/v1701884558/Apple-Logo_luonxp.png"
+                  className="logo-apple"
+                  alt="apple"
+                />
+                <p>Sign in with Apple</p>
+              </div>
             </div>
-        )
-    }
+          </div>
+          <div className="login-part">
+            <label>Email address</label>
+            <input type="mail" className="mail-input" />
+            <label>Password</label>
+            <input type="password" className="password-input" />
+            <p>Forgot Password ?</p>
+            <button>Sign In</button>
+          </div>
+          <p>
+            Don't have an account? <span className="register">Register here</span>
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
-export default Login
+export default Login;
